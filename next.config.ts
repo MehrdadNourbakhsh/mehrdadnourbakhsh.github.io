aba647ed6e301d971cd6f123abf2de73c4886cbd
m.nourbakhsh75 <m.nourbakhsh75@gmail.com>
2025-08-15 16:22:21 +0330

    modify nextjs workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
 
+const nextConfig: NextConfig = {
+  
+  basePath: isProd ? '/portfolio' : '',
   output: 'export',
   images: {
     unoptimized: true,
